ebe83216bbd84bbe715077a6a673aff24008a651
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-06 22:21:26 +0530

    push

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print('Hello, world!')
+#Examples for Variables and Datatypes
+#using strings
+name = 'Rajkumar'
+
+#using integer
+age = 23
+
+#using float
+pi = 3.14
+
+#using list
+cars = ['benz', 'bmw', 'Range Rover']
