70ed495b1e1d61537f7feb7d8fa3e13b6fba1c6a
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-06 21:57:14 +0530

    sample

---
+++ my_app.py
@@ -0,0 +1,16 @@
+# first program
+name = input ('what is your name?')
+print('hellow + name')
+
+#Examples for Variables and Datatypes
+#using strings
+name = 'Rajkumar'
+
+#using integer
+age = 23
+
+#using float
+pi = 3.14
+
+#using list
+cars = ['benz', 'bmw', 'Range Rover']
