c560e201edce514578d05ecfa2487c93cfa72a36
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-07 22:03:31 +0530

    loops

--- lists.py
+++ lists.py
@@ -6,11 +6,4 @@ print(cars[1])
 print(cars[2])
 
 
-# Loops
-cars = ["bmw", "Tata", "tesla"]
 
-for car in cars:
-    if car == "bmw":
-        print = (car.upper())
-    else:
-        print = (car.capitalize())
