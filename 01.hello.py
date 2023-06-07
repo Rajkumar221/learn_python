5cc35e17ad836a9675b2971cc4ecffaf51817850
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-07 20:48:51 +0530

    true

--- 01.hello.py
+++ 01.hello.py
@@ -3,9 +3,18 @@ addition = 10 + 5
 substraction = 10 - 5
 multiplication = 10 * 5
 division = 10 / 5
+mod = 10 % 3
 
 print(addition)
 print(substraction)
 print(multiplication)
 print(division)
+print(mod)
 
+
+# true and false
+print(10 <= 10)
+print(0 == 0)
+print(18 > 5)
+print('Rajkumar'.endswith('g'))
+print('Rajkumar'.endswith('r'))
