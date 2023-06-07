71e5ce9c0f3a85400326cb6e3add91275d8a78e5
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-07 20:59:26 +0530

    state

--- 01.hello.py
+++ 01.hello.py
@@ -18,3 +18,16 @@ print(0 == 0)
 print(18 > 5)
 print('Rajkumar'.endswith('g'))
 print('Rajkumar'.endswith('r'))
+
+
+# Statements
+is_adult = true
+age = 18
+
+if is_adult:
+    print('is adult')
+
+if age >= 18:
+    print('Adult')
+else:
+    print('not an adult')
