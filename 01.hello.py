ce69f4f906de5ad0b019bf130b28cb24c1555d62
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-07 20:38:41 +0530

    art

---
+++ 01.hello.py
@@ -0,0 +1,11 @@
+#  Arithmatic Operations
+addition = 10 + 5
+substraction = 10 - 5
+multiplication = 10 * 5
+division = 10 / 5
+
+print(addition)
+print(substraction)
+print(multiplication)
+print(division)
+
