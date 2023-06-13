09553e874731f7ae66ee81f0bfd8b367dded85f6
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-13 21:01:13 +0530

    s

---
+++ functions.py
@@ -0,0 +1,38 @@
+age = 18
+age2 = 17
+if age < 18:
+    print ('oops not an adult')
+else:
+    print ('hooray i am an adult')
+
+if age2 < 18:
+    print ('oops not an adult')
+else:
+    print ('hooray i am an adult')
+
+
+# using def function
+age = 18
+age2 = 17
+
+def check_age(age):
+    if age < 18:
+         print ('oops not an adult')
+    else:
+        print ('hooray i am an adult')
+
+check_age(age)
+check_age(age2)
+
+#2
+def check_age(age):
+    if age < 18:
+         print ('oops not an adult')
+    else:
+        print ('hooray i am an adult')
+
+check_age(18)
+check_age(17)
+check_age(99)
+
+#Built in functions
--- loops.py
+++ loops.py
@@ -1,6 +1,17 @@
-cars = ["bmw", "Tata", "tesla"]
+cars = ["bmw", "Tata", "tesla", "toyota", "honda"]
 for car in cars:
-    if car == "bmw":
-        print = (car.upper())
+    if car == 'bmw':
+        print(car.upper())
     else:
-        print = (car.capitalize())
+        print (car.capitalize())
+
+
+
+#while loop
+number = 0
+
+while number <=10:
+    print (number)
+    number = number + 1
+else:
+    print ('while loop is ended and value of number is ' + str(number))
